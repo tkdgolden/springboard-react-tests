2b3131f97761f05d1def871b8302d64fc9843cdf
Olivia Golden <olivia.golden.w@gmail.com>
2024-03-04 23:45:59 -0600

    failing left arrow test

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -53,5 +53,38 @@ describe("Carousel", () => {
       container.querySelector('img[alt="testing image 2"]')
     ).toBeInTheDocument();
   });
+  
+  // left arrow
+  it("works when you click on the right arrow", function () {
+    const { container } = render(
+      <Carousel
+        photos={TEST_IMAGES}
+        title="images for testing"
+      />
+    );
+    // move forward in the carousel
+    const rightArrow = container.querySelector(".bi-arrow-right-circle");
+    fireEvent.click(rightArrow);
+
+    // expect the second image to show, but not the first
+    expect(
+      container.querySelector('img[alt="testing image 2"]')
+    ).toBeInTheDocument();
+    expect(
+      container.querySelector('img[alt="testing image 1"]')
+    ).not.toBeInTheDocument();
+
+    // move forward in the carousel
+    const leftArrow = container.querySelector(".bi-arrow-left-circle");
+    fireEvent.click(leftArrow);
+
+    // expect the first image to show, but not the second
+    expect(
+      container.querySelector('img[alt="testing image 2"]')
+    ).not.toBeInTheDocument();
+    expect(
+      container.querySelector('img[alt="testing image 1"]')
+    ).toBeInTheDocument();
+  });
 })
 
